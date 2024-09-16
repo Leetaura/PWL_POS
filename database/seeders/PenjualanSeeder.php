d731ad47016765392db3db60f87f2ca2d1d86f0d
= <=>
2024-09-16 21:44:04 +0700

    seeder berusaha

---
+++ database/seeders/PenjualanSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PenjualanSeedeer extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
