a28c9801b48078887d5cc318bbb1871d027af735
= <=>
2024-09-30 20:20:12 +0700

    ke 2.1 pratikum

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,21 +6,87 @@ use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
+// class UserController extends Controller
+// {
+//     public function index()
+//     {
+//         $data = [
+//             'level_id' => 2,
+//             'username' => 'manager_tiga',
+//             'nama' => 'Manager 3',
+//             'password' => Hash::make('12345')
+//         ];
+//         UserModel::create($data);
+//         // Ambil semua data pengguna dari database
+//         $user = UserModel::all();
+
+//         // Tampilkan data pengguna dalam view 'user'
+//         return view('user',['data' => $user]);
+//     }
+// }
+
+// class UserController extends Controller
+// {
+//     public function index()
+//     {
+//         // Ambil semua data pengguna dari database
+//         $user = UserModel::find(1);
+
+//         // Tampilkan data pengguna dalam view 'user'
+//         return view('user',['data' => $user]);
+//     }
+// }
+
+// class UserController extends Controller
+// {
+//     public function index()
+//     {
+//         // Ambil semua data pengguna dari database
+//         $user = UserModel::where('level_id', 1)->first();
+
+//         // Tampilkan data pengguna dalam view 'user'
+//         return view('user',['data' => $user]);
+//     }
+// }
+
+// class UserController extends Controller
+// {
+//     public function index()
+//     {
+//         // Ambil semua data pengguna dari database
+//         $user = UserModel::firstWhere('level_id', 1);
+
+//         // Tampilkan data pengguna dalam view 'user'
+//         return view('user',['data' => $user]);
+//     }
+// }
+
+// class UserController extends Controller
+// {
+//     public function index()
+//     {
+//         // Ambil semua data pengguna dari database
+//         $user = UserModel::findOr(20, ['username', 'nama',], function (){
+//             abort(404);
+//         });
+
+//         // Tampilkan data pengguna dalam view 'user'
+//         return view('user',['data' => $user]);
+//     }
+// }
+
 class UserController extends Controller
 {
     public function index()
     {
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
         // Ambil semua data pengguna dari database
-        $user = UserModel::all();
+        $user = UserModel::findOr(20, ['username', 'nama',], function (){
+            abort(404);
+        });
 
         // Tampilkan data pengguna dalam view 'user'
         return view('user',['data' => $user]);
     }
 }
+
+
